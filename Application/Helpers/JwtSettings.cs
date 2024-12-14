55e939cc7b2da9f7a4b85c11cbdc60deb44e8ad3
kedr4 <85400754+kedr4@users.noreply.github.com>
2024-12-15 02:50:37 +0300

    Fixes

--- Application/Helpers/JwtSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application.Helpers;
-
-public class JwtSettings()
-{
-    public string Secret { get; set; } = string.Empty;
-    public string Issuer { get; set; } = string.Empty;
-    public string Audience { get; set; } = string.Empty;
-
-}
